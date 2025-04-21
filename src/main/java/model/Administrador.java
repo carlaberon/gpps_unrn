cc9f47fc6237d64e24dcdd9a2018c71c30ec6608
Carla Berón <alracnoreb@gmail.com>
2025-04-21 12:02:06 -0300

    usuarios

--- src/main/java/model/Administrador.java
+++ src/main/java/model/Administrador.java
@@ -1,7 +1,5 @@
 package model;
 
 public class Administrador extends Usuario {
-    public Administrador(int id, String nombreUsuario, String contrasenia, String nombre, String email) {
-        super(id, nombreUsuario, contrasenia, nombre, email);
     }
 }
