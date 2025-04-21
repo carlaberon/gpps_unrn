bf9e262663b0994dceba61b505837e80e0757bde
Carla Berón <alracnoreb@gmail.com>
2025-04-21 11:21:39 -0300

    main

--- src/main/java/main/Main.java
+++ src/main/java/main/Main.java
@@ -2,8 +2,5 @@ package main;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("hello world");
-        System.out.println("ver si funciona");
-        System.out.println("ver si funciona intento 2");
     }
 }
