9e280b248816cde6ed1dad2a2eacc7d836664898
EzeRamos17 <hernan_ram99@hotmail.com>
2025-05-01 18:03:36 -0300

    primer commit

---
+++ src/main/java/model/Director.java
@@ -0,0 +1,7 @@
+package model;
+
+public class Director extends Usuario {
+    public Director(String nombreUsuario, String contrasenia, String nombre, String email) {
+        super(nombreUsuario, contrasenia, nombre, email);
+    }
+}
