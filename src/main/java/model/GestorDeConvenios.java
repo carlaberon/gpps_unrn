7cd5214536b3e3211d68a74de3154edc08ecf240
Carla Berón <alracnoreb@gmail.com>
2025-05-15 19:15:27 -0300

    gestor de convenios

---
+++ src/main/java/model/GestorDeConvenios.java
@@ -0,0 +1,24 @@
+package model;
+import java.io.InputStream;
+
+public class GestorDeConvenios {
+    void create(Convenio convenio);
+    void upDateArchivo(Convenio convenio);
+    void cargarArchivoConvenioFirmado(int idConvenio, InputStream archivoPDF);
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+ */
