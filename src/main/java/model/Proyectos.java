f8d289babb419a350fdf4781dd0bcc7435244417
Carla Berón <alracnoreb@gmail.com>
2025-05-15 18:52:29 -0300

    realizo HU: proponer proyecto + test

---
+++ src/main/java/model/Proyectos.java
@@ -0,0 +1,15 @@
+package model;
+
+public class Proyectos {
+    private GestorDeProyectos gestorDeProyectos;
+
+    public Proyectos(GestorDeProyectos gestorDeProyectos){
+        this.gestorDeProyectos = gestorDeProyectos;
+    }
+    public void propuestaDeProyecto( int id, String nombre, String descripcion, Boolean estado, String areaDeInteres,
+                                     Estudiante estudiante, Director director, Tutor docenteSupervisor){
+        var proyecto = new Proyecto(id, nombre, descripcion, estado, areaDeInteres, estudiante, director, docenteSupervisor);
+        this.gestorDeProyectos.registrarPropuestaDeProyecto(proyecto);
+        //registra el proyecto
+    }
+}
