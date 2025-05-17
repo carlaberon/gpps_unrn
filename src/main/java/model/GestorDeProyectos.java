c9f7387f82f4904bfcc883530e43ea8602c01104
Carla Berón <alracnoreb@gmail.com>
2025-05-17 19:10:24 -0300

    agrego: cargar informe parcial y final + valorar informe

--- src/main/java/model/GestorDeProyectos.java
+++ src/main/java/model/GestorDeProyectos.java
@@ -5,4 +5,8 @@ public interface GestorDeProyectos {
     void registrarPropuestaDeProyecto(Proyecto proyecto);
 
     void registrarAsignacionDocenteTutor(int idProyecto, int idDocente, int idTutor);
+
+    void cargarInformeParcial(Informe informeParcial);
+
+    void cargarInformeFinal(Informe informeFinal);
 }
