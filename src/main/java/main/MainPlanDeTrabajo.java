333e6c41b0cd1e3abe357c0c2912fdd6cf519470
TomasRoldann <tomikpp_roldan_186@hotmail.com>
2025-06-08 15:48:53 -0300

    ventana arreglada
    asdsad

--- src/main/java/main/MainPlanDeTrabajo.java
+++ src/main/java/main/MainPlanDeTrabajo.java
@@ -2,12 +2,14 @@ package main;
 
 import database.ServicioDePersistenciaGestionProyectos;
 import ui.VerProyecto;
+import ui.VerProyectoInformes;
 
 public class MainPlanDeTrabajo {
 
     public static void main(String[] args) {
 //        new CrearPlanTrabajo(new ServicioDePersistenciaGestionProyectos(), 1).setVisible(true);
         new VerProyecto(new ServicioDePersistenciaGestionProyectos(), 1).setVisible(true);
+        new VerProyectoInformes(new ServicioDePersistenciaGestionProyectos(), 1).setVisible(true);
 //        new ProyectosACargo(new ServicioDePersistenciaGestionProyectos(), 5);
 //        new AprobacionDeProyectos(new ServicioDePersistenciaGestionProyectos());
 
